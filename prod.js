2d1f5ebbf25190a53754abc1462733c58677ac59
rachid MBoutou <SARAH GAX@Rachid.titan.lan>
2025-08-29 17:00:53 +0100

    mon 4 rpos

--- prod.js
+++ prod.js
@@ -1,8 +1,6 @@
-const BtnCommander = document.querySelector(".btn2")
+const BtnCommander = document.querySelector(".btn-commande");
 
-BtnCommander.addEventListener("click", () =>{
-  commanderWhatsApp();
-})
+let pageUrl = window.location.href; // Déclaration ici
 
 // Si tu es en local, remplace par ton lien GitHub
 if (pageUrl.includes("localhost")) {
@@ -12,14 +10,12 @@ if (pageUrl.includes("localhost")) {
 const encodedUrl = encodeURIComponent(pageUrl);
 const messageCommande = encodeURIComponent("Je souhaite commander ce produit : ");
 
-// function commanderFacebook() {
-//   window.open(`https://www.facebook.com/sharer/sharer.php?u=${pageUrl}&quote=${messageCommande}`, '_blank', 'noopener,noreferrer');
-// }
-
-// function commanderTwitter() {
-//   window.open(`https://twitter.com/intent/tweet?url=${pageUrl}&text=${messageCommande}`, '_blank', 'noopener,noreferrer');
-// }
-
 function commanderWhatsApp() {
-  window.open(`https://api.whatsapp.com/send?text=${messageCommande}%20${pageUrl}`, '_blank', 'noopener,noreferrer');
+  window.open(`https://api.whatsapp.com/send?text=${messageCommande}%20${encodedUrl}`, '_blank', 'noopener,noreferrer');
+}
+
+if (BtnCommander) {
+  BtnCommander.addEventListener("click", () => {
+    commanderWhatsApp();
+  });
 }
